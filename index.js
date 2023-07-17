e4cdbd2979e05e0399341897c0978430ce4b589b
monty-cristo <ruymbeeckjonah@gmail.com>
2023-07-17 18:50:47 +0200

    Added changes and comments

--- index.js
+++ index.js
@@ -1,42 +1,57 @@
-const counterDisplay = document.querySelector("h3");
-let counter = 0,
-  currentCounter;
+const scoreDisplay = document.querySelector("#score")
 
-const bubbleMaker = () => {
-  clearInterval(currentCounter);
-  currentCounter = setInterval(() => {
-    const bubble = document.createElement("span");
-    bubble.classList.add("bubble");
-    document.body.appendChild(bubble);
+const btnPause = document.querySelector("#pause")
+const btnPlay = document.querySelector("#play")
+const btnReset = document.querySelector("#reset")
 
-    const size = Math.random() * 200 + 100 + "px";
-    bubble.style.height = size;
-    bubble.style.width = size;
+let score = 0
+let interval
 
-    bubble.style.top = Math.random() * 100 + 50 + "%";
-    bubble.style.left = Math.random() * 100 + "%";
+btnPause.addEventListener("click", pause)
+btnPlay.addEventListener("click", play)
+btnReset.addEventListener("click", reset)
 
-    const plusMinus = Math.random() > 0.5 ? 1 : -1;
-    bubble.style.setProperty("--left", Math.random() * 100 * plusMinus + "%");
+function play() {
+  clearInterval(interval)
+
+  interval = setInterval(() => {
+    const bubble = document.createElement("span")
+    bubble.classList.add("bubble")
+    document.body.appendChild(bubble)
+
+    // Using the new css typed object model instead of the old untyped one
+    // https://developer.chrome.com/blog/cssom
+    // It has quite a few benefits but browser support probably isn't there yet
+    const size = CSS.vmax(Math.random() * 15 + 5)
+    bubble.attributeStyleMap.set("width", size)
+    bubble.attributeStyleMap.set("height", size)
+
+    bubble.attributeStyleMap.set("bottom", CSS.percent(Math.random() * 20 + 5))
+    bubble.attributeStyleMap.set("left", CSS.percent(Math.random() * 100))
+
+    const plusMinus = Math.random() > 0.5 ? 1 : -1
+    // Custom properties still have to use the string version which is kinda annoying
+    bubble.attributeStyleMap.set("--x-translation", `${Math.random() * 100 * plusMinus}vw`)
 
     bubble.addEventListener("click", () => {
-      counter++;
-      counterDisplay.textContent = counter;
-      bubble.remove();
-    });
+      score++
+      scoreDisplay.textContent = score
+      bubble.remove()
+    })
 
     setTimeout(() => {
-      bubble.remove();
-    }, 8000);
-  }, 300);
-};
+      bubble.remove()
+    }, 8000)
+  }, 300)
+}
 
-const pause = () => {
-  clearInterval(currentCounter);
-};
 
-const reset = () => {
-  pause();
-  counter = 0;
-  counterDisplay.textContent = counter;
-};
+function pause() {
+  clearInterval(interval)
+}
+
+function reset() {
+  pause()
+  score = 0
+  scoreDisplay.textContent = score
+}
